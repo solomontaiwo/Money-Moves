9c1b46518d4c1ad5e214fbea54d15d10098abd9b
solomontaiwo <solomonolamide.taiwo@edu.unife.it>
2024-01-06 19:03:17 +0100

    Altre inizializzazioni

--- database/migrations/2024_01_06_175100_create_expenses_table.php
+++ database/migrations/2024_01_06_175100_create_expenses_table.php
@@ -8,19 +8,16 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('expenses', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+            $table->text('category');
+            $table->double('amount');
         });
     }
 
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('expenses');
     }
 };
